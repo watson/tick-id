9b55b3bc16825bcbd72452d32f7397ecfff6f681
Thomas Watson Steen <w@tson.dk>
2015-09-23 10:38:01 +0200

    Use closure (don't code while sleeping)

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ if (!process.addAsyncListener) require('async-listener')
 
 var noop = function () {}
 
-global._tickId = 1
+var tickId = 1
 
 process.addAsyncListener({
   create: noop,
   before: function (context, data) {
-    global._tickId = global._tickId < MAX_SAFE_INTEGER ? global._tickId + 1 : 1
+    tickId = tickId < MAX_SAFE_INTEGER ? tickId + 1 : 1
   },
   error: noop,
   after: noop
 })
 
 module.exports = function () {
-  return global._tickId
+  return tickId
 }
